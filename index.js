bc42574fb6d4c75d82ad0e369b226f2faa0b2e28
Sage Weil <sage@newdream.net>
2021-10-21 09:32:43 -0500

    multiple

--- index.js
+++ index.js
@@ -45,8 +45,10 @@ const axios = require('axios');
 	    const msg = errors.join('; ');
 	    console.log(`Checklist incomplete: ${msg}`);
 	    core.setOutput("errors", msg);
-	    core.error(msg);
 	    core.setFailed(msg);
+	    for (const err of errors) {
+		core.error(err);
+	    }
 	}
     } catch (error) {
 	console.log(`Error: ${error.message}`);
