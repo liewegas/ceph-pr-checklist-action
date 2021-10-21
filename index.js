090d1b728b287ec6f6e5c26bf588a7504fdd4b29
Sage Weil <sage@newdream.net>
2021-10-21 09:11:22 -0500

    more

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+//const core = require('@actions/core');
+//const github = require('@actions/github');
+const axios = require('axios');
 
-try {
-  const description = core.getInput('description');
-  console.log(`Desc is ${description}!`);
-  core.setOutput("errors", null);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async() => {
+    try {
+	const url = github.context.payload['pull_request']['_links']['self']['href'];
+	//const url = 'https://api.github.com/repos/liewegas/doorpi/pulls/1';
+	
+	const r = await axios.get(url);
+	const lines = r.data.body.split('\n');
+	
+	console.log('lines');
+	console.log(`lines are "${lines[0]}"`);
+	let errors = [];
+	for (var i = 0; i < lines.length; ++i) {
+	    lines[i] = lines[i].replace(/(\r\n|\n|\r)/gm, "");
+	}
+	for (var i = 0; i < lines.length; ++i) {
+	    console.log(`line ${i} is "${lines[i]}"`);
+	    if (lines[i] == 'Checklist'
+		&& i < lines.length - 2
+		&& lines[i+1][0] == '=') {
+		i += 2;
+		while (lines[i][0] == '-') {
+		    const section = lines[i].substring(2).split(' (')[0];
+		    ++i;
+		    if (i >= lines.length) break;
+		    let checked = false;
+		    while (lines[i].substring(0, 5) == '  - [' &&
+			   lines[i][6] == ']') {
+			if (lines[i][5] == 'x') {
+			    checked = true;
+			}
+			++i;
+			if (i >= lines.length) break;
+		    }
+		    console.log(`section ${section} ... checked = ${checked}`);
+		    if (checked) {
+			errors.push(`Must check an item from ${section}`);
+		    }
+		}
+	    }
+	}
+
+	if (errors.length) {
+	    console.log(`Errors: ${errors.join('; ')}`);
+	    core.setOutput("errors", errors.join('; '));
+	}
+    } catch (error) {
+	console.log(`Error: ${error.message}`);
+	core.setFailed(error.message);
+    }
+})();
